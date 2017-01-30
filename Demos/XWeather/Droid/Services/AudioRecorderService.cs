99e405f82ed547d534fb151add5778d15fb7e142
Nate Rickard <nate.rickard@microsoft.com>
2017-01-30 12:01:11 -0500

    formatting

--- Demos/XWeather/Droid/Services/AudioRecorderService.cs
+++ Demos/XWeather/Droid/Services/AudioRecorderService.cs
@@ -73,8 +73,10 @@ namespace XWeather.WeatherBot
 			int readResult = 0;
 			DateTime? silenceTime = null;
 
-			if (os != null) {
-				while (isRecording) {
+			if (os != null)
+			{
+				while (isRecording)
+				{
 					readResult = recorder.Read (data, 0, bufferSize);
 
 					if (readResult > 0)// Xamarin seems to have normalized this buffer read like most .NET reads, readResult == the # bytes read, so we can't compare to TrackStatus.Success, etc.
@@ -85,30 +87,41 @@ namespace XWeather.WeatherBot
 						{
 							audioDetected = true;
 							silenceTime = null;
-						} else //no audio detected
-						  {
+						}
+						else //no audio detected
+						{
 							//see if we've detected 'near' silence for more than <audioTimeout>
-							if (endOnSilence && silenceTime.HasValue) {
-								if (DateTime.Now.Subtract (silenceTime.Value) > audioTimeout) {
+							if (endOnSilence && silenceTime.HasValue)
+							{
+								if (DateTime.Now.Subtract (silenceTime.Value) > audioTimeout)
+								{
 									StopRecording ();
 								}
-							} else {
+							}
+							else
+							{
 								silenceTime = DateTime.Now;
 							}
 						}
 
 						//write the data to disk
-						try {
+						try
+						{
 							os.Write (data);
-						} catch (Exception ex) {
+						}
+						catch (Exception ex)
+						{
 							System.Diagnostics.Debug.WriteLine (ex.Message);
 						}
 					}
 				}
 
-				try {
+				try
+				{
 					os.Close ();
-				} catch (Exception ex) {
+				}
+				catch (Exception ex)
+				{
 					System.Diagnostics.Debug.WriteLine (ex.Message);
 				}
 
@@ -119,7 +132,8 @@ namespace XWeather.WeatherBot
 
 		public void StopRecording ()
 		{
-			if (recorder != null) {
+			if (recorder != null)
+			{
 				isRecording = false;
 
 				recorder.Stop ();
@@ -143,42 +157,57 @@ namespace XWeather.WeatherBot
 			//bool signed = (RECORDER_AUDIO_ENCODING == Android.Media.Encoding. AudioFormat. Encoding.PCM_SIGNED);
 			bool bigEndian = false;// (format.isBigEndian());
 
-			if (use16Bit) {
-				for (int i = readPoint; i < buffer.Length - leftOver; i += 2) {
+			if (use16Bit)
+			{
+				for (int i = readPoint; i < buffer.Length - leftOver; i += 2)
+				{
 					int value = 0;
 					// deal with endianness
 					int hiByte = (bigEndian ? buffer [i] : buffer [i + 1]);
 					int loByte = (bigEndian ? buffer [i + 1] : buffer [i]);
 
-					if (signed) {
+					if (signed)
+					{
 						short shortVal = (short)hiByte;
 						shortVal = (short)((shortVal << 8) | (byte)loByte);
 						value = shortVal;
-					} else {
+					}
+					else
+					{
 						value = (hiByte << 8) | loByte;
 					}
 					max = Math.Max (max, value);
 				} // for
-			} else {
+			}
+			else
+			{
 				// 8 bit - no endianness issues, just sign
-				for (int i = readPoint; i < buffer.Length - leftOver; i++) {
+				for (int i = readPoint; i < buffer.Length - leftOver; i++)
+				{
 					int value = 0;
 
-					if (signed) {
+					if (signed)
+					{
 						value = buffer [i];
-					} else {
+					}
+					else
+					{
 						short shortVal = 0;
 						shortVal = (short)(shortVal | buffer [i]);
 						value = shortVal;
 					}
+
 					max = Math.Max (max, value);
 				} // for
 			} // 8 bit
 			  // express max as float of 0.0 to 1.0 of max value
 			  // of 8 or 16 bits (signed or unsigned)
-			if (signed) {
+			if (signed)
+			{
 				if (use16Bit) { level = (float)max / MAX_16_BITS_SIGNED; } else { level = (float)max / MAX_8_BITS_SIGNED; }
-			} else {
+			}
+			else
+			{
 				if (use16Bit) { level = (float)max / MAX_16_BITS_UNSIGNED; } else { level = (float)max / MAX_8_BITS_UNSIGNED; }
 			}
 
@@ -221,7 +250,8 @@ namespace XWeather.WeatherBot
 
 			byte [] data = new byte [bufferSize];
 
-			try {
+			try
+			{
 				input = new FileInputStream (tempFile);
 				output = new FileOutputStream (permanentFile);
 				totalAudioLen = input.Channel.Size ();
@@ -231,13 +261,16 @@ namespace XWeather.WeatherBot
 
 				WriteWaveFileHeader (output, totalAudioLen, totalDataLen, longSampleRate, channels, byteRate);
 
-				while (input.Read (data) != -1) {
+				while (input.Read (data) != -1)
+				{
 					output.Write (data);
 				}
 
 				input.Close ();
 				output.Close ();
-			} catch (Exception ex) {
+			}
+			catch (Exception ex)
+			{
 				System.Diagnostics.Debug.WriteLine (ex.Message);
 			}
 		}
